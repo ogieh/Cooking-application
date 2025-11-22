affa4ec73caf7c82af7e5f42352635bcf0b7223e
Gabriel <125802297+ogieh@users.noreply.github.com>
2025-11-22 13:31:46 +0100

    added another function

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ def my_function(a, b):
     print("Hello Ella and Hadassah")
     print(a+b)
 my_function(5, 5)
+
+def another_function(x):
+    return x * x
+result = another_function(4)
+print(result)
