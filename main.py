4829cf07aaaa8b37006eb60ed28f01c11f3e502d
Gabriel <125802297+ogieh@users.noreply.github.com>
2026-03-23 11:57:41 +0100

    cooking application
    mobile app test 101

--- main.py
+++
@@ -1,9 +0,0 @@
-def my_function(a, b):
-    print("Hello Ella and Hadassah")
-    print(a+b)
-my_function(5, 5)
-
-def another_function(x):
-    return x * x
-result = another_function(4)
-print(result)
